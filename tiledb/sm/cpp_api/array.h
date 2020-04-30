fe1833020644e3d0768eca623626097c9fbccd8d
Isaiah Norton <isaiah@tiledb.io>
2020-04-30 09:04:25 -0400

    Add C ptr constructor for ::Array and ::Ctx in C++ API

--- tiledb/sm/cpp_api/array.h
+++ tiledb/sm/cpp_api/array.h
@@ -284,6 +284,32 @@ class Array {
             timestamp) {
   }
 
+  /**
+   * Constructor. Creates a TileDB Array instance wrapping the given pointer.
+   * @param ctx tiledb::Context
+   * @param own=true If false, disables underlying cleanup upon destruction.
+   * @throws TileDBError if construction fails
+   */
+  Array(const Context& ctx, tiledb_array_t* array, bool own = true)
+      : ctx_(ctx)
+      , schema_(ArraySchema(ctx, (tiledb_array_schema_t*)nullptr)) {
+    if (array == nullptr)
+      throw TileDBError(
+          "[TileDB::C++API] Error: Failed to create Array from pointer");
+
+    tiledb_ctx_t* c_ctx = ctx.ptr().get();
+
+    tiledb_array_schema_t* array_schema;
+    ctx.handle_error(tiledb_array_get_schema(c_ctx, array, &array_schema));
+    schema_ = ArraySchema(ctx, array_schema);
+
+    array_ = std::shared_ptr<tiledb_array_t>(array, [own](tiledb_array_t* p) {
+      if (own) {
+        tiledb_array_free(&p);
+      }
+    });
+  }
+
   Array(const Array&) = default;
   Array(Array&&) = default;
   Array& operator=(const Array&) = default;
