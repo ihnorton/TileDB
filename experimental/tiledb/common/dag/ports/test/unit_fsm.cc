31e7f5738083194d068d2523a05b55fc8e7dae63
Andrew Lumsdaine <andrew.lumsdaine@tiledb.com>
2022-06-18 14:07:15 -0700

    initial ports implementation and unit tests

--- experimental/tiledb/common/dag/ports/test/unit_fsm.cc
+++ experimental/tiledb/common/dag/ports/test/unit_fsm.cc
@@ -31,6 +31,10 @@
  */
 
 #include "unit_fsm.h"
+#include <condition_variable>
+#include <future>
+#include <iostream>
+#include <mutex>
 #include "experimental/tiledb/common/dag/ports/fsm.h"
 
 using namespace tiledb::common;
@@ -38,118 +42,323 @@ using namespace tiledb::common;
 TEST_CASE("Port FSM: Construct", "[fsm]") {
   [[maybe_unused]] auto a = PortStateMachine{};
 
+  CHECK(a.state() == PortState::start_start);
+}
+
+TEST_CASE("Port FSM: Start up", "[fsm]") {
+  [[maybe_unused]] auto a = PortStateMachine{};
+
+  CHECK(a.state() == PortState::start_start);
+
+  SECTION("start source") {
+    a.event(PortEvent::source_start);
+    CHECK(a.state() == PortState::empty_start);
+  }
+
+  SECTION("start sink") {
+    a.event(PortEvent::sink_start);
+    CHECK(a.state() == PortState::start_full);
+  }
+}
+
+TEST_CASE("Port FSM: Basic manual sequence", "[fsm]") {
+  [[maybe_unused]] auto a = PortStateMachine{};
+
+  CHECK(a.state() == PortState::start_start);
+  a.event(PortEvent::source_start);
+  a.event(PortEvent::sink_start);
+
+  a.event(PortEvent::source_data_fill);
+  CHECK(str(a.state()) == "full_full");
+  a.event(PortEvent::sink_data_drain);
+  CHECK(str(a.state()) == "ready_empty");
+  a.event(PortEvent::sink_swap);
+  CHECK(str(a.state()) == "empty_full");
+
+  a.event(PortEvent::sink_data_drain);
+  CHECK(str(a.state()) == "empty_empty");
+
+  CHECK(str(a.state()) == "empty_empty");
+  a.event(PortEvent::source_data_fill);
+  CHECK(str(a.state()) == "full_ready");
+  a.event(PortEvent::source_swap);
+  CHECK(a.state() == PortState::empty_full);
+
+  a.event(PortEvent::sink_data_drain);
+  CHECK(a.state() == PortState::empty_empty);
+
+  a.event(PortEvent::source_data_fill);
+  CHECK(str(a.state()) == "full_ready");
+  a.event(PortEvent::source_swap);
+  CHECK(str(a.state()) == "empty_full");
+  a.event(PortEvent::source_data_fill);
+  CHECK(str(a.state()) == "full_full");
+  a.event(PortEvent::sink_data_drain);
+  CHECK(str(a.state()) == "ready_empty");
+  a.event(PortEvent::source_swap);
+  CHECK(str(a.state()) == "empty_full");
+
+  a.event(PortEvent::sink_data_drain);
+  CHECK(a.state() == PortState::empty_empty);
+
+  a.event(PortEvent::source_data_fill);
+  CHECK(str(a.state()) == "full_ready");
+  a.event(PortEvent::source_swap);
+  CHECK(str(a.state()) == "empty_full");
+  a.event(PortEvent::source_data_fill);
+  CHECK(str(a.state()) == "full_full");
+  a.event(PortEvent::sink_data_drain);
+  CHECK(str(a.state()) == "ready_empty");
+  a.event(PortEvent::sink_swap);
+  CHECK(str(a.state()) == "empty_full");
+
+  a.event(PortEvent::sink_data_drain);
+  CHECK(a.state() == PortState::empty_empty);
+
+  a.event(PortEvent::source_data_fill);
+  CHECK(str(a.state()) == "full_ready");
+  a.event(PortEvent::sink_swap);
+  CHECK(str(a.state()) == "empty_full");
+  a.event(PortEvent::source_data_fill);
+  CHECK(str(a.state()) == "full_full");
+  a.event(PortEvent::sink_data_drain);
+  CHECK(str(a.state()) == "ready_empty");
+  a.event(PortEvent::source_swap);
+  CHECK(str(a.state()) == "empty_full");
+
+  a.event(PortEvent::sink_data_drain);
+  CHECK(a.state() == PortState::empty_empty);
+
+  a.event(PortEvent::source_data_fill);
+  CHECK(str(a.state()) == "full_ready");
+  a.event(PortEvent::sink_swap);
+  CHECK(str(a.state()) == "empty_full");
+  a.event(PortEvent::source_data_fill);
+  CHECK(str(a.state()) == "full_full");
+  a.event(PortEvent::sink_data_drain);
+  CHECK(str(a.state()) == "ready_empty");
+  a.event(PortEvent::sink_swap);
+  CHECK(str(a.state()) == "empty_full");
+
+  a.event(PortEvent::sink_data_drain);
   CHECK(a.state() == PortState::empty_empty);
 }
 
-TEST_CASE("Port FSM: Source sequence", "[fsm]") {
+std::string is_src_empty(PortState st) {
+  if (str(st) == "empty_start" || str(st) == "empty_empty" ||
+      str(st) == "empty_full" || str(st) == "empty_ready") {
+    return {};
+  }
+  return str(st);
+}
+
+std::string is_src_full(PortState st) {
+  if (str(st) == "full_start" || str(st) == "full_empty" ||
+      str(st) == "full_full" || str(st) == "full_ready") {
+    return {};
+  }
+  return str(st);
+}
+
+std::string is_src_ready(PortState st) {
+  if (str(st) == "ready_start" || str(st) == "ready_empty" ||
+      str(st) == "ready_full" || str(st) == "ready_ready") {
+    return {};
+  }
+  return str(st);
+}
+
+std::string is_src_swappable(PortState st) {
+  if (str(st) == "ready_start" || str(st) == "ready_empty" ||
+      str(st) == "ready_full" || str(st) == "ready_ready") {
+    return {};
+  }
+  return str(st);
+}
+
+std::string is_snk_empty(PortState st) {
+  if (str(st) == "start_empty" || str(st) == "empty_empty" ||
+      str(st) == "full_empty" || str(st) == "ready_empty") {
+    return {};
+  }
+  return str(st);
+}
+
+std::string is_snk_full(PortState st) {
+  if (str(st) == "start_full" || str(st) == "empty_full" ||
+      str(st) == "full_full" || str(st) == "ready_full") {
+    return {};
+  }
+  return str(st);
+}
+
+std::string is_snk_ready(PortState st) {
+  if (str(st) == "start_ready" || str(st) == "empty_ready" ||
+      str(st) == "full_ready" || str(st) == "ready_ready") {
+    return {};
+  }
+  return str(st);
+}
+
+TEST_CASE("Port FSM: Asynchronous source and sink", "[fsm]") {
+  constexpr bool debug = false;
+
   [[maybe_unused]] auto a = PortStateMachine{};
 
-  CHECK(a.state() == PortState::empty_empty);
-  a.event(PortEvent::src_data_fill);
+  CHECK(str(a.state()) == "start_start");
 
-  CHECK(a.state() == PortState::full_empty);
+  std::mutex mutex_;
+  std::condition_variable source_cv, sink_cv;
 
-  a.event(PortEvent::source_filled);
+  size_t rounds = 33;
 
-  CHECK(a.state() == PortState::full_ready);
+  /**
+   * Function to emulate a source node
+   *
+   * The basic steps/states are:
+   *   start -> state: empty_any
+   *   while (true)
+   *     invoke producer function
+   *     submit -> state: full_any
+   *     wait
+   *       sink could swap -> state = empty_full
+   *       sink could drain -> full_empty or ready_empty
+   *     if (full_empty || ready_empty)
+   *       swap -> empty_full
+   */
+  auto source_node = [&]() {
+    size_t n = rounds;
 
-  a.event(PortEvent::sink_drained);
+    {
+      std::unique_lock lock(mutex_);
+      a.event(PortEvent::source_start, debug ? "source" : "");
+      CHECK(is_src_empty(a.state()) == "");
+    }
 
-  CHECK(a.state() == PortState::ready_ready);
+    while (n--) {
+      std::unique_lock lock(mutex_);
 
-  a.event(PortEvent::source_swap);
+      a.event(PortEvent::source_data_fill, debug ? "source" : "");
+      CHECK(is_src_full(a.state()) == "");
+      sink_cv.notify_one();
+      source_cv.wait(lock);
 
-  CHECK(a.state() == PortState::empty_full);
+      if (/*a.state() == PortState::ready_ready ||*/
+          a.state() == PortState::ready_empty ||
+          a.state() == PortState::full_empty) {
+        if (debug)
+          std::cout << "source swapping " << str(a.state()) << std::endl;
 
-  a.event(PortEvent::src_data_fill);
+        a.event(PortEvent::source_swap, debug ? "source" : "");
+        sink_cv.notify_one();
 
-  CHECK(a.state() == PortState::full_full);
+        CHECK(is_src_empty(a.state()) == "");
+        CHECK(is_snk_full(a.state()) == "");
+      } else {
+        if (debug)
 
-  a.event(PortEvent::sink_data_drain);
+          std::cout << "source try_swap: "
+                    << port_state_strings[static_cast<int>(a.state())]
+                    << std::endl;
 
-  CHECK(a.state() == PortState::full_empty);
+        CHECK(is_src_empty(a.state()) == "");
+      }
+    }
+  };
 
-  a.event(PortEvent::source_filled);
+  /**
+   * Function to emulate a sink node
+   *
+   * The basic steps/states are:
+   *   start -> state: any_full
+   *   drain -> state: any_empty
+   *   while (true)
+   *     wait
+   *       source could swap -> state = empty_full
+   *       source could fill -> full_empty or full_ready
+   *     if (full_empty || full_ready)
+   *       swap -> empty_full
+   *     invoke consumer function
+   *     drain -> state: any_empty
+   */
+  auto sink_node = [&]() {
+    size_t n = rounds;
 
-  CHECK(a.state() == PortState::full_ready);
+    {
+      std::unique_lock lock(mutex_);
+      a.event(PortEvent::sink_start, debug ? "sink" : "");
+      CHECK(is_snk_full(a.state()) == "");
+      a.event(PortEvent::sink_data_drain, debug ? "sink" : "");
+      CHECK(is_snk_empty(a.state()) == "");
+      source_cv.notify_one();
+    }
 
-  a.event(PortEvent::sink_swap);
+    while (n--) {
+      std::unique_lock lock(mutex_);
 
-  CHECK(a.state() == PortState::empty_full);
+      sink_cv.wait(lock);
 
-  a.event(PortEvent::sink_data_drain);
+      if (debug)
 
-  CHECK(a.state() == PortState::empty_empty);
+        std::cout << "sink coming out of wait  " << str(a.state()) << std::endl;
 
-  a.event(PortEvent::src_data_fill);
+      if (/*a.state() == PortState::ready_ready || */
+          a.state() == PortState::full_ready ||
+          a.state() == PortState::full_empty) {
+        a.event(PortEvent::sink_swap, debug ? "sink" : "");
 
-  CHECK(a.state() == PortState::full_empty);
+        CHECK(is_src_empty(a.state()) == "");
+        CHECK(is_snk_full(a.state()) == "");
 
-  a.event(PortEvent::sink_drained);
+        source_cv.notify_one();
+      } else {
+        if (debug)
 
-  CHECK(a.state() == PortState::ready_empty);
+          std::cout << "sink try swap " << str(a.state()) << std::endl;
 
-  a.event(PortEvent::source_swap);
+        CHECK(is_snk_full(a.state()) == "");
+      }
 
-  CHECK(a.state() == PortState::empty_full);
+      if (debug)
+        std::cout << "sink retrieving" << std::endl;
+      a.event(PortEvent::sink_data_drain, debug ? "sink" : "");
+      CHECK(is_snk_empty(a.state()) == "");
+      source_cv.notify_one();
+    }
+  };
 
-  a.event(PortEvent::src_data_fill);
+  SECTION("launch source before sink, get source before sink") {
+    auto fut_a = std::async(std::launch::async, source_node);
+    auto fut_b = std::async(std::launch::async, sink_node);
 
-  CHECK(a.state() == PortState::full_full);
+    fut_a.get();
+    fut_b.get();
+  }
 
-  a.event(PortEvent::sink_data_drain);
+  SECTION("launch sink before source, get source before sink") {
+    auto fut_b = std::async(std::launch::async, sink_node);
+    auto fut_a = std::async(std::launch::async, source_node);
 
-  CHECK(a.state() == PortState::full_empty);
+    fut_a.get();
+    fut_b.get();
+  }
 
-  a.event(PortEvent::source_filled);
+  SECTION("launch source before sink, get sink before source") {
+    auto fut_a = std::async(std::launch::async, source_node);
+    auto fut_b = std::async(std::launch::async, sink_node);
 
-  CHECK(a.state() == PortState::full_ready);
+    fut_b.get();
+    fut_a.get();
+  }
 
-  a.event(PortEvent::sink_swap);
+  SECTION("launch sink before source, get sink before source") {
+    auto fut_b = std::async(std::launch::async, sink_node);
+    auto fut_a = std::async(std::launch::async, source_node);
 
-  CHECK(a.state() == PortState::empty_full);
+    fut_b.get();
+    fut_a.get();
+  }
 
-  a.event(PortEvent::src_data_fill);
-
-  CHECK(a.state() == PortState::full_full);
-
-  a.event(PortEvent::sink_data_drain);
-
-  CHECK(a.state() == PortState::full_empty);
-
-  a.event(PortEvent::source_filled);
-
-  CHECK(a.state() == PortState::full_ready);
-
-  a.event(PortEvent::sink_drained);
-
-  CHECK(a.state() == PortState::ready_ready);
-
-  a.event(PortEvent::sink_swap);
-
-  CHECK(a.state() == PortState::empty_full);
-
-  a.event(PortEvent::sink_data_drain);
-
-  CHECK(a.state() == PortState::empty_empty);
-
-  a.event(PortEvent::src_data_fill);
-
-  CHECK(a.state() == PortState::full_empty);
-
-  a.event(PortEvent::sink_drained);
-
-  CHECK(a.state() == PortState::ready_empty);
-
-  a.event(PortEvent::source_filled);
-
-  CHECK(a.state() == PortState::ready_ready);
-
-  a.event(PortEvent::sink_swap);
-
-  CHECK(a.state() == PortState::empty_full);
-
-  a.event(PortEvent::sink_data_drain);
-
-  CHECK(a.state() == PortState::empty_empty);
+  CHECK(is_src_empty(a.state()) == "");
+  CHECK(is_snk_empty(a.state()) == "");
 }
