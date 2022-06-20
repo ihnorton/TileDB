f368b940d0f7cbcf7a88894708b69ae3a499cdab
Andrew Lumsdaine <andrew.lumsdaine@tiledb.com>
2022-06-20 12:11:16 -0700

    updated documentation, increased unit test coverage for fsm [skip ci]

--- experimental/tiledb/common/dag/ports/fsm.h
+++ experimental/tiledb/common/dag/ports/fsm.h
@@ -1,5 +1,5 @@
 /**
- * @file   ports.h
+ * @file   fsm.h
  *
  * @section LICENSE
  *
@@ -41,56 +41,89 @@
 
 namespace tiledb::common {
 
-enum class PortEvent : unsigned short {
-  source_fill = 0,
-  swap,
-  sink_drain,
-  shutdown
-};
-
-namespace {
-inline constexpr unsigned short to_index(PortEvent x) {
-  return static_cast<unsigned short>(x);
-}
-
-/**
- * Number of events in the PortEvent state machine
- */
-constexpr unsigned int n_events = to_index(PortEvent::shutdown) + 1;
-
-static std::vector<std::string> event_strings{
-    "source_fill",
-    "swap",
-    "sink_drain",
-    "shutdown",
-};
-
-static auto str(PortEvent ev) {
-  return event_strings[static_cast<int>(ev)];
-}
-
-}  // namespace
-
 // clang-format off
 /**
+ * This is the state transition table for a simple state machine for two
+ * communicating ports.  Each port has two states, empty or full.  There are
+ * three events: source_fill, sink_drain, and swap.  For simplicty there are
+ * currently no defined events for startup, stop, forced shutdown, or abort
+ * (though we have an event enum that anticipates shutdown).
  *
- *    +-----------------+-------------------------------------------------------------+
- *    |      States     |                           Events                            |
- *    +--------+--------+------------------+-------------+-----------------+----------+
- *    | Source |  Sink  | source_fill      | swap        | sink_drain      | shutdown |
- *    |--------+--------+------------------+-------------+-----------------+----------+
- *    | empty  | empty  | full/empty       |             |                 |          |
- *    |--------+--------+------------------+-------------+-----------------+----------+
- *    | empty  | full   | full/full        |             | empty/empty     |          |
- *    |--------+--------+------------------+-------------+-----------------+----------+
- *    | full   | empty  |                  | empty/full  |                 |          |
- *    |--------+--------+------------------+-------------+-----------------+----------+
- *    | full   | full   |                  |             | full/empty      |          |
- *    +--------+--------+------------------+-------------+-----------------+----------+
+ *    +-----------------+---------------------------------------------------+
+ *    |      States     |                    Events                         |
+ *    +--------+--------+-------------+------------+-------------+----------+
+ *    | Source |  Sink  | source_fill | swap       | sink_drain  | shutdown |
+ *    |--------+--------+-------------+------------+-------------+----------+
+ *    | empty  | empty  | full/empty  |            |             |          |
+ *    |--------+--------+-------------+------------+-------------+----------+
+ *    | empty  | full   | full/full   |            | empty/empty |          |
+ *    |--------+--------+-------------+------------+-------------+----------+
+ *    | full   | empty  |             | empty/full |             |          |
+ *    |--------+--------+-------------+------------+-------------+----------+
+ *    | full   | full   |             |            | full/empty  |          |
+ *    +--------+--------+-------------+------------+-------------+----------+
+ *
+ *
+ *
+ * The basic behavior of a user of a source port is
+ *   init: { state == empty_full }
+ *   start: { state == empty_full /\ state == empty_empty }
+ *   while (not done)
+ *     loop_begin: { state == empty_empty /\ state == empty_full }
+ *     pre_produce: { state == empty_empty /\ state == empty_full }
+ *     invoke producer function
+ *     during producer function, sink could have drained
+ *     source_fill: { state == full_empty /\ state == full_full }
+ *     sink.notify()
+ *     wait
+ *     post wait, the following could have happened
+ *       sink could have swapped -> state = empty_full
+ *       sink could have drained -> state = full_empty
+ *     post_wait: { state == full_empty /\ state == empty_full }
+ *     if (state == full_empty)
+ *       pre_swap: { state ==  full_empty }
+ *       swap elements
+ *       post_swap: { state ==  empty_full }
+ *       sink.notify()
+ *     else
+ *       else_swap: { state == empty_empty \/ state == empty_full
+ *   post_loop: { state == empty_empty \/ state == empty_full }
+ *
+ *
+ * The basic behavior of a user of a sink port is
+ *   We perform some initializetion to get into a proper starting state
+ *   init: { state == empty_full /\ state == full_full }
+ *   drain: { state == empty_empty /\ state == full_empty }
+ *   start: { state == empty_empty /\ state == full_empty }
+ *   source.notify()
+ *   while (not done)
+ *     loop_begin: { state == empty_empty /\ state == full_empty }
+ *     wait
+ *     post wait, the following could have happened
+ *       source could have swapped -> state = empty_full
+ *       source could have filled  -> full_empty
+ *     post_wait: { state == full_empty /\ state == empty_full }
+ *     if (state == full_empty)
+ *       pre_swap: { state == full_empty }
+ *       swap
+ *       post_swap: { state ==  empty_full }
+ *       source.notify()
+ *     else
+ *       else_swap: { state == full_empty /\ state == empty_full }
+ *     pre_consume: { state == empty_full /\ state == full_full }
+ *     invoke consumer function
+ *     during consumer function, source could have filled
+ *     drain: { state == empty_empty /\ state == full_empty }
+ *     source.notify()
+ *  post_loop: { state = empty_empty }
+ *
  *
  */
 // clang-format on
 
+/**
+ * An enum representing the different states of the bound ports
+ */
 enum class PortState {
   empty_empty,
   empty_full,
@@ -104,12 +137,16 @@ namespace {
 constexpr unsigned short to_index(PortState x) {
   return static_cast<unsigned short>(x);
 }
+
 /**
  * Number of states in the Port state machine
  */
 constexpr unsigned short n_states = to_index(PortState::done) + 1;
 }  // namespace
 
+/**
+ * Strings for each enum member, for debugging.
+ */
 static std::vector<std::string> port_state_strings{
     "empty_empty",
     "empty_full",
@@ -119,17 +156,51 @@ static std::vector<std::string> port_state_strings{
     "done",
 };
 
+/**
+ * Function to convert a state to a string.
+ *
+ * @param event The event to stringify.
+ * @return The string corresponding to the event.
+ */
 static auto str(PortState st) {
   return port_state_strings[static_cast<int>(st)];
 }
 
-enum class PortAction : unsigned short {
-  none = 0,
-  fill_source,
-  drain_sink,
+enum class PortEvent : unsigned short {
+  source_fill,
   swap,
+  sink_drain,
   shutdown
 };
+
+inline constexpr unsigned short to_index(PortEvent x) {
+  return static_cast<unsigned short>(x);
+}
+
+/**
+ * Number of events in the PortEvent state machine
+ */
+constexpr unsigned int n_events = to_index(PortEvent::shutdown) + 1;
+
+/**
+ * Strings for each enum member, for debugging.
+ */
+static std::vector<std::string> event_strings{
+    "source_fill",
+    "swap",
+    "sink_drain",
+    "shutdown",
+};
+
+/**
+ * Function to convert event to a string.
+ *
+ * @param event The event to stringify.
+ * @return The string corresponding to the event.
+ */
+static auto str(PortEvent ev) {
+  return event_strings[static_cast<int>(ev)];
+}
 
 /**
  * State transition table.
@@ -140,12 +211,19 @@ enum class PortAction : unsigned short {
  * source_filled are really the same event, as are sink_drained and
  * sink_drain. Without start events, there are also no start states.
  *
- * Todo: Protect state transitions with mutex
+ * The original fsm also included a "ready" state for source and sink, but this
+ * was absorbed into "full" for source and "empty" for sink since the transition
+ * from ready to those states wwas "always", i.e., it did not require an actual
+ * event for that state transition to happen.  Moreover, the state of the source
+ * and sink is really just the state of the item it contains.
+ *
  */
 namespace {
+
 // clang-format off
   constexpr PortState transition_table[n_states][n_events] {
   /* source_sink */    /* source_fill */      /* swap */             /* sink_drain */        /* shutdown */
+
   /* empty_empty */  { PortState::full_empty, PortState::error,      PortState::error,       PortState::error },
   /* empty_full  */  { PortState::full_full,  PortState::error,      PortState::empty_empty, PortState::error },
   /* full_empty  */  { PortState::error,      PortState::empty_full, PortState::error,       PortState::error },
@@ -154,24 +232,17 @@ namespace {
   /* error       */  { PortState::error,      PortState::error,      PortState::error,       PortState::error },
   /* done        */  { PortState::error,      PortState::error,      PortState::error,       PortState::error },
   };
+// clang-format on
 
-  /**
-   * Transition actions table.
-   */
-  constexpr PortAction action_table[n_states][n_events] {
-  /* source_sink */    /*source_fill */        /* swap */            /* sink_drain */        /* shutdown */
-  /* empty_empty */  { PortAction::none,       PortAction::none,     PortAction::none,       PortAction::none },
-  /* empty_full  */  { PortAction::none,       PortAction::none,     PortAction::none,       PortAction::none },
-  /* full_empty  */  { PortAction::none,       PortAction::none,     PortAction::none,       PortAction::none },
-  /* full_full   */  { PortAction::none,       PortAction::none,     PortAction::none,       PortAction::none },
+  }  // namespace
 
-  /* error       */  { PortAction::none,       PortAction::none,     PortAction::none,       PortAction::none },
-  /* done        */  { PortAction::none,       PortAction::none,     PortAction::none,       PortAction::none },
-  };
-
-  // clang-format on
-}  // namespace
-
+/**
+ * Class representing states of a bound source and sink node.
+ * The class is agnostic as to how the events are actually
+ * implemented by the users of the the state machine.
+ *
+ * Todo: Derived class to represent bound nodes.
+ */
 class PortStateMachine {
  private:
   PortState state_;
@@ -183,14 +254,35 @@ class PortStateMachine {
   PortStateMachine()
       : state_(PortState::empty_full){};
 
+  /**
+   * Return the current state
+   */
   [[nodiscard]] inline PortState state() const {
     return state_;
   }
 
  public:
+  /**
+   * Function to handle state transitions based on external events.
+   *
+   * Todo: Protect state transitions with mutex?
+   *
+   * Todo: Make event() private and create do_fill(), do_drain(),
+   * and do_swap() public members.
+   *
+   * Todo: Investigate coroutine-like approach for corresponding with external
+   * events so that the procession of steps is driven by the state machine
+   * rather than the user of the state machine.
+   *
+   * Some code that prints state information is currently included for debugging
+   * purposes.
+   *
+   * @param event The event to be processed
+   * @param event msg A debugging string to preface printout information for the
+   * state transition
+   */
   void event(PortEvent event, const std::string msg = "") {
     auto new_state{transition_table[to_index(state_)][to_index(event)]};
-    auto action{action_table[to_index(state_)][to_index(event)]};
 
     if (msg != "") {
       std::cout << msg + " " + str(event) + ": " + str(state_) + " -> " +
@@ -198,41 +290,12 @@ class PortStateMachine {
                 << std::endl;
     }
 
-    switch (action) {
-      case PortAction::fill_source:
-        break;
-
-      case PortAction::drain_sink:
-        break;
-
-      case PortAction::swap:
-        break;
-
-      case PortAction::none:
-        break;
-
-      default:
-        throw std::logic_error("Port state action not reachable");
-    }
     /*
-     * Assign new state only after action returns without throwing.
+     * Assign new state
      */
     state_ = new_state;
   }
-
-  /*
-   * Try to apply source / sink event
-   */
-  bool try_swap() {
-    auto old_state = state_;
-    event(PortEvent::swap);
-    if (old_state == state_) {
-      return false;
-    }
-    return true;
-  }
 };
-
 }  // namespace tiledb::common
 
 #endif  // TILEDB_DAG_FSM_H
--- experimental/tiledb/common/dag/ports/test/unit_fsm.cc
+++ experimental/tiledb/common/dag/ports/test/unit_fsm.cc
@@ -146,62 +146,50 @@ TEST_CASE("Port FSM: Basic manual sequence", "[fsm]") {
 }
 
 std::string is_src_empty(PortState st) {
-  if (str(st) == "empty_start" || str(st) == "empty_empty" ||
-      str(st) == "empty_full" || str(st) == "empty_ready") {
+  if (str(st) == "empty_full" || str(st) == "empty_empty") {
     return {};
   }
   return str(st);
 }
 
 std::string is_src_full(PortState st) {
-  if (str(st) == "full_start" || str(st) == "full_empty" ||
-      str(st) == "full_full" || str(st) == "full_ready") {
+  if (str(st) == "full_full" || str(st) == "full_empty") {
     return {};
   }
   return str(st);
 }
 
-std::string is_src_ready(PortState st) {
-  if (str(st) == "ready_start" || str(st) == "ready_empty" ||
-      str(st) == "ready_full" || str(st) == "ready_ready") {
-    return {};
-  }
-  return str(st);
-}
-
-std::string is_src_swappable(PortState st) {
-  if (str(st) == "ready_start" || str(st) == "ready_empty" ||
-      str(st) == "ready_full" || str(st) == "ready_ready") {
+std::string is_src_post_swap(PortState st) {
+  if (str(st) == "full_empty" || str(st) == "empty_full" ||
+      str(st) == "empty_empty") {
     return {};
   }
   return str(st);
 }
 
 std::string is_snk_empty(PortState st) {
-  if (str(st) == "start_empty" || str(st) == "empty_empty" ||
-      str(st) == "full_empty" || str(st) == "ready_empty") {
+  if (str(st) == "full_empty" || str(st) == "empty_empty") {
     return {};
   }
   return str(st);
 }
 
 std::string is_snk_full(PortState st) {
-  if (str(st) == "start_full" || str(st) == "empty_full" ||
-      str(st) == "full_full" || str(st) == "ready_full") {
+  if (str(st) == "full_full" || str(st) == "empty_full") {
     return {};
   }
   return str(st);
 }
 
-std::string is_snk_ready(PortState st) {
-  if (str(st) == "start_ready" || str(st) == "empty_ready" ||
-      str(st) == "full_ready" || str(st) == "ready_ready") {
+std::string is_snk_post_swap(PortState st) {
+  if (str(st) == "full_empty" || str(st) == "empty_full" ||
+      str(st) == "full_full") {
     return {};
   }
   return str(st);
 }
 
-size_t random_ms(size_t max = 7) {
+size_t random_us(size_t max = 7500) {
   thread_local static uint64_t generator_seed =
       std::hash<std::thread::id>()(std::this_thread::get_id());
   thread_local static std::mt19937_64 generator(generator_seed);
@@ -219,144 +207,208 @@ TEST_CASE("Port FSM: Asynchronous source and sink", "[fsm]") {
   std::mutex mutex_;
   std::condition_variable source_cv, sink_cv;
 
-  size_t rounds = 33;
+  size_t rounds = 37;
   if (debug)
     rounds = 3;
 
   int source_item{0};
   int sink_item{0};
 
+  int source_swaps{};
+  int sink_swaps{};
+
   /**
-   * Function to emulate an asynchronous source node
+   * The basic behavior of a user of a source port is described in fsm.h.
    *
-   * The basic steps/states are:
-   *   start -> state: empty_any
-   *   while (true)
-   *     invoke producer function
-   *     submit -> state: full_any
-   *     wait
-   *       sink could swap -> state = empty_full
-   *       sink could drain -> full_empty or ready_empty
-   *     if (full_empty || ready_empty)
-   *       swap -> empty_full
+   * This test function loops for a given number of rounds, executing the
+   * workflow shown in fsm.h. CHECKs are made for each of the predicates
+   * therein.
    */
   auto source_node = [&]() {
     size_t n = rounds;
 
+    // Take a really big lock to keep events atomic with checks.
+    std::unique_lock lock(mutex_);
+
+    // start: { state == empty_full /\ state == empty_empty }
+    CHECK(is_src_empty(a.state()) == "");
+
     // Event loop for source
     while (n--) {
-      std::unique_lock lock(mutex_);
-
+      // Begin production of new item
+      // loop_begin: { state == empty_empty /\ state == empty_full }
+      CHECK(is_src_empty(a.state()) == "");
       {
         if (debug)
           std::cout << "source filling " << str(a.state()) << std::endl;
 
+        // pre_produce: { state == empty_empty /\ state == empty_full }
+        CHECK(is_src_empty(a.state()) == "");
         lock.unlock();
-        std::this_thread::sleep_for(std::chrono::milliseconds(random_ms(19)));
+        std::this_thread::sleep_for(std::chrono::microseconds(random_us(7500)));
         lock.lock();
       }
 
       CHECK(source_item == 0);
+      source_item = 1;
+
       a.event(PortEvent::source_fill, debug ? "source" : "");
 
-      source_item = 1;
+      // source_fill: { state == full_empty /\ state == full_full }
       CHECK(is_src_full(a.state()) == "");
       sink_cv.notify_one();
       source_cv.wait(lock);
 
+      // post_wait: { state == full_empty \/ state == empty_full \/
+      //             state == empty_empty }
+      CHECK(is_src_post_swap(a.state()) == "");
+
       if (a.state() == PortState::full_empty) {
+        // pre_swap: { state == full_empty }
+        CHECK(str(a.state()) == "full_empty");
         if (debug)
           std::cout << "source swapping " << str(a.state()) << std::endl;
 
         a.event(PortEvent::swap, debug ? "source" : "");
+        source_swaps++;
+
+        // post_swap: { state == empty_full }
+        CHECK(is_src_empty(a.state()) == "");
+        CHECK(is_snk_full(a.state()) == "");
+
+        // { source_item == 1 /\ sink_item == 0 }
         CHECK(source_item == 1);
         CHECK(sink_item == 0);
         std::swap(source_item, sink_item);
-        sink_cv.notify_one();
 
-        CHECK(is_src_empty(a.state()) == "");
-        CHECK(is_snk_full(a.state()) == "");
+        // { source_item == 0 /\ sink_item == 1 }
+        CHECK(source_item == 0);
+        CHECK(sink_item == 1);
+
+        sink_cv.notify_one();
       } else {
+        // else_swap: { state == empty_empty \/ state == empty_full }
+        CHECK(is_src_empty(a.state()) == "");
+
         if (debug)
 
           std::cout << "source try_swap: "
                     << port_state_strings[static_cast<int>(a.state())]
                     << std::endl;
 
-        CHECK(is_src_empty(a.state()) == "");
+        // { source_item == 0 }
         CHECK(source_item == 0);
       }
     }
+
+    // post_loop: { state == empty_empty }
+    CHECK(is_src_empty(a.state()) == "");
+
+    // { source_item == 0 }
+    CHECK(source_item == 0);
   };
 
   /**
-   * Function to emulate an asynchronous sink node
+   * The basic behavior of a user of a sink port is described in fsm.h.
    *
-   * The basic steps/states are:
-   *   start -> state: any_full
-   *   drain -> state: any_empty
-   *   while (true)
-   *     wait
-   *       source could swap -> state = empty_full
-   *       source could fill -> full_empty or full_ready
-   *     if (full_empty || full_ready)
-   *       swap -> empty_full
-   *     invoke consumer function
-   *     drain -> state: any_empty
+   * This test function loops for a given number of rounds, executing the
+   * workflow shown in fsm.h. CHECKs are made for each of the predicates
+   * therein.
    */
   auto sink_node = [&]() {
     size_t n = rounds;
 
-    {
-      std::unique_lock lock(mutex_);
-      a.event(PortEvent::sink_drain, debug ? "sink" : "");
-      CHECK(is_snk_empty(a.state()) == "");
-      source_cv.notify_one();
-    }
+    // Take a really big lock to keep events atomic with checks.
+    std::unique_lock lock(mutex_);
+
+    /**
+     * Initialization for sink_node, to emulate it having been filled (necessary
+     * to avoid deadlocking).
+     */
+    // init: { state == empty_full /\ state == full_full }
+    CHECK(is_snk_full(a.state()) == "");
+
+    a.event(PortEvent::sink_drain, debug ? "sink" : "");
+    CHECK(is_snk_empty(a.state()) == "");
+    source_cv.notify_one();
 
     // Event loop for sink
     while (n--) {
-      std::unique_lock lock(mutex_);
+      // loop_begin: { state == empty_empty /\ state == full_empty }
+      CHECK(is_snk_empty(a.state()) == "");
+
       sink_cv.wait(lock);
+
+      // post_wait: { state == full_empty \/ state == empty_full \/
+      //              state == full_full }
+      CHECK(is_snk_post_swap(a.state()) == "");
 
       if (debug)
         std::cout << "sink coming out of wait  " << str(a.state()) << std::endl;
 
       if (a.state() == PortState::full_empty) {
-        CHECK(source_item == 1);
-        CHECK(sink_item == 0);
+        // pre_swap: { state == full_empty }
+        CHECK(str(a.state()) == "full_empty");
         a.event(PortEvent::swap, debug ? "sink" : "");
-        std::swap(source_item, sink_item);
 
+        // post_swap: { state == full_empty }
         CHECK(is_src_empty(a.state()) == "");
         CHECK(is_snk_full(a.state()) == "");
 
+        // { source_item == 1 /\ sink_item == 0 }
+        CHECK(source_item == 1);
+        CHECK(sink_item == 0);
+        std::swap(source_item, sink_item);
+        sink_swaps++;
+
+        // { source_item == 0 /\ sink_item == 1 }
+        CHECK(source_item == 0);
+        CHECK(sink_item == 1);
+
         source_cv.notify_one();
       } else {
+        // else_swap: { state == empty_full \/ state == full_full }
+        CHECK(is_snk_full(a.state()) == "");
+
         if (debug)
           std::cout << "sink try swap " << str(a.state()) << std::endl;
 
-        CHECK(is_snk_full(a.state()) == "");
+        // { sink_item == 1 }
         CHECK(sink_item == 1);
       }
 
+      // pre_consume: { state == empty_full /\ state == full_full }
+      CHECK(is_snk_full(a.state()) == "");
       {
         if (debug)
           std::cout << "sink retrieving " << str(a.state()) << std::endl;
 
         lock.unlock();
-        std::this_thread::sleep_for(std::chrono::milliseconds(random_ms(19)));
+        std::this_thread::sleep_for(std::chrono::microseconds(random_us(7500)));
         lock.lock();
       }
-
       a.event(PortEvent::sink_drain, debug ? "sink" : "");
+
+      // drain: { state == empty_empty /\ state == full_empty }
       CHECK(is_snk_empty(a.state()) == "");
+
       CHECK(sink_item == 1);
       sink_item = 0;
       source_cv.notify_one();
     }
+    // post_loop: { state == empty_empty }
+    CHECK(str(a.state()) == "empty_empty");
+
+    // { source_item == 0 /\ sink_item == 0 }
+    CHECK(source_item == 0);
+    CHECK(sink_item == 0);
   };
 
+  /**
+   * Test asynchronous execution of source and sink functions.  We use different
+   * combinations of orderings for launching the asynchronous tasks, as well as
+   * for waiting on their completion.  For now we use std::async.
+   */
   SECTION("launch source before sink, get source before sink") {
     auto fut_a = std::async(std::launch::async, source_node);
     auto fut_b = std::async(std::launch::async, sink_node);
@@ -389,8 +441,31 @@ TEST_CASE("Port FSM: Asynchronous source and sink", "[fsm]") {
     fut_a.get();
   }
 
+  /** Final check after each section
+   *
+   * The final state should be empty for both source and sink and both items
+   * should be empty.
+   *
+   */
+  // { state == empty_empty }
+  CHECK(str(a.state()) == "empty_empty");
   CHECK(is_src_empty(a.state()) == "");
   CHECK(is_snk_empty(a.state()) == "");
+
+  // { source_item == 0 /\ sink_item == 0 }
   CHECK(source_item == 0);
   CHECK(sink_item == 0);
+
+  /**
+   * Check that there were an expected number of swaps between the source and
+   * the sink.  In debug mode, print out the number of each.  Over multiple
+   * runs, we expect the number of each to change.
+   */
+  if (debug) {
+    std::cout << source_swaps << " source_swaps and " << sink_swaps
+              << " sink_swaps" << std::endl;
+    CHECK((source_swaps + sink_swaps) == 3);
+  } else {
+    CHECK((source_swaps + sink_swaps) == 37);
+  }
 }
