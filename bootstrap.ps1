6fc22daee2b34b298f7b8219a6b79d9a309fe635
Isaiah Norton <isaiah@tiledb.io>
2022-11-28 11:42:43 -0500

    WIP

--- bootstrap.ps1
+++ bootstrap.ps1
@@ -94,7 +94,7 @@ Param(
     [string]$Prefix,
     [string]$Dependency,
     [string]$CMakeGenerator,
-    [switch]$CMakeExtraArgs,
+    [string[]]]$CMakeExtraArgs,
     [switch]$EnableAssert,
     [switch]$EnableDebug,
     [switch]$EnableReleaseSymbols,
@@ -250,10 +250,12 @@ if ($PSBoundParameters.ContainsKey("CMakeGenerator")) {
     $GeneratorFlag = "-G""$CMakeGenerator"""
 }
 
-# Set CMake generator type.
+# Set CMake extra arguments.
+$GeneratorFlag = ""
 $ExtraArgsFlag = ""
 if ($PSBoundParameters.ContainsKey("CMakeExtraArgs")) {
-    $ExtraArgsFlag = "$CMakeExtraArgs"
+    $CMakeExtraArgsArray = [String[]]($CMakeExtraArgs -split ',' | ForEach { -join("-D", $_.Replace("'", "")) })
+    $CMakeExtraArgsFlag = [String]($CMakeExtraArgsArray -join("$_ ") )
 }
 
 # Enforce out-of-source build
@@ -271,7 +273,7 @@ if ($CMakeGenerator -eq $null) {
 
 # Run CMake.
 # We use Invoke-Expression so we can echo the command to the user.
-$CommandString = "cmake -A X64 -DCMAKE_BUILD_TYPE=$BuildType -DCMAKE_INSTALL_PREFIX=""$InstallPrefix"" -DCMAKE_PREFIX_PATH=""$DependencyDir"" -DMSVC_MP_FLAG=""/MP$BuildProcesses"" -DTILEDB_ASSERTIONS=$AssertionMode -DTILEDB_VERBOSE=$Verbosity -DTILEDB_AZURE=$UseAzure -DTILEDB_S3=$UseS3 -DTILEDB_SERIALIZATION=$UseSerialization -DTILEDB_WERROR=$Werror -DTILEDB_CPP_API=$CppApi -DTILEDB_TESTS=$Tests -DTILEDB_STATS=$Stats -DTILEDB_STATIC=$TileDBStatic -DTILEDB_FORCE_ALL_DEPS=$TileDBBuildDeps -DTILEDB_TOOLS=$TileDBTools -DTILEDB_EXPERIMENTAL_FEATURES=$TileDBExperimentalFeatures -DTILEDB_WEBP=$BuildWebP -DTILEDB_ABSEIL=$BuildAbseil -DTILEDB_CRC32=$BuildCrc32 $GeneratorFlag ""$ExtraArgsFlag"" ""$SourceDirectory"""
+$CommandString = "cmake -A X64 -DCMAKE_BUILD_TYPE=$BuildType -DCMAKE_INSTALL_PREFIX=""$InstallPrefix"" -DCMAKE_PREFIX_PATH=""$DependencyDir"" -DMSVC_MP_FLAG=""/MP$BuildProcesses"" -DTILEDB_ASSERTIONS=$AssertionMode -DTILEDB_VERBOSE=$Verbosity -DTILEDB_AZURE=$UseAzure -DTILEDB_S3=$UseS3 -DTILEDB_SERIALIZATION=$UseSerialization -DTILEDB_WERROR=$Werror -DTILEDB_CPP_API=$CppApi -DTILEDB_TESTS=$Tests -DTILEDB_STATS=$Stats -DTILEDB_STATIC=$TileDBStatic -DTILEDB_FORCE_ALL_DEPS=$TileDBBuildDeps -DTILEDB_TOOLS=$TileDBTools -DTILEDB_EXPERIMENTAL_FEATURES=$TileDBExperimentalFeatures -DTILEDB_WEBP=$BuildWebP -DTILEDB_ABSEIL=$BuildAbseil -DTILEDB_CRC32=$BuildCrc32 $GeneratorFlag ""$CMakeExtraArgsFlag"" ""$SourceDirectory"""
 Write-Host $CommandString
 Write-Host
 Invoke-Expression "$CommandString"
