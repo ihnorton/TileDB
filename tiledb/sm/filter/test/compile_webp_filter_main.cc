71be0bd2cdd831fe819f056e74b6d6e4fcad3e7a
Isaiah Norton <isaiah@tiledb.io>
2023-01-03 22:11:04 -0500

    Fix

--- tiledb/sm/filter/test/compile_webp_filter_main.cc
+++ tiledb/sm/filter/test/compile_webp_filter_main.cc
@@ -29,10 +29,6 @@
 #include "tiledb/sm/filter/webp_filter.h"
 
 int main() {
-<<<<<<< Updated upstream
   tiledb::sm::WebpFilter();
-=======
-  (void)sizeof(tiledb::sm::WebpFilter);
->>>>>>> Stashed changes
   return 0;
 }
