1958c48cdaae052f3ab9e939c846cf814bce821e
Isaiah Norton <isaiah@tiledb.io>
2023-05-24 09:00:22 -0400

    Fix static linkage with object stores enabled

--- cmake/Modules/FindAWSSDK_EP.cmake
+++ cmake/Modules/FindAWSSDK_EP.cmake
@@ -39,6 +39,23 @@ if(TILEDB_VCPKG)
   #       it lists bare "pthread;curl" which leads to linkage of system versions. For static linkage, we
   #       handle those elsewhere at the moment.
   find_package(AWSSDK REQUIRED QUIET COMPONENTS ${AWS_SERVICES})
+
+  if (TILEDB_STATIC)
+
+    # not included for unknown reasons
+    list(APPEND AWSSDK_THIRD_PARTY_LIBS aws-c-io aws-c-cal)
+
+    set(AWSSDK_EXTRA_LIBS)
+    foreach(TARGET IN LISTS AWSSDK_THIRD_PARTY_LIBS)
+        message(STATUS "Try finding ${TARGET}")
+        find_package(${TARGET} REQUIRED NO_DEFAULT_PATH)
+        message(STATUS "Found ${TARGET}")
+        list(APPEND AWSSDK_EXTRA_LIBS "AWS::${TARGET}")
+    endforeach()
+    #message(FATAL_ERROR "f '${AWSSDK_EXTRA_LIBS}'")
+    install_all_target_libs("${AWSSDK_EXTRA_LIBS}")
+  endif()
+
   install_all_target_libs("${AWSSDK_LINK_LIBRARIES}")
   return()
 endif()
